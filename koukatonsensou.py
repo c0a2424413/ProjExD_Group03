e6b079f437a889912d7c11cc15cc8dc75a251c1b
c0a240289a <c0a0289a@edu.teu.ac.jp>
2025-07-08 18:43:53 +0900

    追加機能2とりあえず実装

--- koukatonsensou.py
+++ koukatonsensou.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import random
+import time
 
 
 os.path.dirname(os.path.abspath(__file__))
@@ -44,12 +45,39 @@ class EnemyUnit:
     def draw(self):
         pygame.draw.rect(screen, (255, 100, 100), (self.x, self.y, 30, 30))
 
+
+class Buff:
+    """
+    味方ユニットにバフを掛ける機能に関するクラス
+    """
+    def __init__(self,cats,screen):
+        """
+        画面内にいる味方ユニットの速度と与えるダメージを増加させる。また画面を緑に光らせる。
+        引数 screen : 画面Surface
+        引数2 cats : catsリスト
+        """
+        for cat in cats:
+            cat.speed = 2  # スピードを1から2に変更
+            cat.damege = 6  # 与えるダメージを5から6に変更
+        buff_img = pygame.Surface((WIDTH, HEIGHT)) 
+        for i in range(20):
+            pygame.draw.rect(buff_img,(0,255,100),(0,0,WIDTH,HEIGHT))  # 画面全体が緑色に光る
+            buff_img.set_alpha(8*(i+1))
+            screen.blit(buff_img,[0,0])
+            pygame.display.update()
+            time.sleep(0.01)
+
+
 # リスト
-cats = []
+cats = []  # buffクラスで使用(catsを)
 enemies = []
 
 # タイマー
 enemy_spawn_timer = 0
+
+# バフをかけられるようになるまでのタイマー
+buff_timer = 1000
+buff_st = "no"
 
 # メインループ
 running = True
@@ -65,11 +93,30 @@ while running:
             if event.key == pygame.K_SPACE:
                 cats.append(CatUnit())
 
+        if event.type == pygame.KEYDOWN:
+                if buff_st == "yes":                 
+                    if event.key == pygame.K_b:
+                        Buff(cats,screen)
+                        buff_st = "no"
+
     # 敵を定期的に出す
     enemy_spawn_timer += 1
     if enemy_spawn_timer > 120:  # 2秒ごとに敵を出す
         enemies.append(EnemyUnit())
         enemy_spawn_timer = 0
+
+    buff_timer += 1  # 1/60秒に1増加
+    # print(buff_timer)
+    if buff_timer > 1200:  # 20秒ごとにバフをかけられるようにする。
+        buff_st = "yes"
+        buff_timer = 0  # タイマーリセット
+    
+    if buff_st == "yes":  # バフをかけられるか判定 
+        buff_ok_font = pygame.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体",20)  # fontでバフをかけられるかどうか示す(文字の描画)
+        txt = buff_ok_font.render("bキーでバフ!",True, (0, 0, 0))
+        buff_ok_rct = txt.get_rect()
+        buff_ok_rct.center = (80,40)
+        screen.blit(txt,buff_ok_rct)
 
     # ユニットの動き・描画
     for cat in cats:
